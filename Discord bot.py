b29042fb1a1a5ee738d19d27c93148f0ff9fb5de
Tinos Psomadakis <46272051+pTinosq@users.noreply.github.com>
2019-06-10 01:51:30 +0100

    NOT STABLE - Just saving b4 bed :)

--- Discord bot.py
+++ Discord bot.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import json
 import tiquations as tq
 import ast
+import random
+import datetime
 
 ConfigLoc = 'config.json'
 r=open(ConfigLoc,"r")
@@ -10,6 +12,7 @@ data=json.load(r)
 ConfigJSON=list(data.values())
 PREFIX=ConfigJSON[0]
 TOKEN=ConfigJSON[1]
+token='cDus2Wi70KJ0pgEt.7r0l1ed.sK1d.qOl6fsDA.b7ICHPit.FxC1Zk8LE9m.eZ3n6dxs'
 WEATHERTOKEN=ConfigJSON[2]
 availableroles=[]
 client = commands.Bot(command_prefix=PREFIX)
@@ -21,13 +24,14 @@ with open("roles.txt","r") as s:
             availableroles.append(x[:-1])
 
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 async def check_if_server(ctx):
     #if await check_if_server(ctx):
-    if message.guild.id == 586587331654320152:
+    if ctx.message.guild.id == 586587331654320152:
         return True
     else:
         return False
@@ -51,6 +55,86 @@ async def hello(ctx):
     await ctx.message.channel.send("Hello!")
 
 @client.command(pass_context=True)
+async def ticket(ctx, *info):
+    if await check_if_server(ctx):
+        channelr = client.get_channel(587426608265035776)
+        personID=ctx.message.author.id
+        personName=str(ctx.message.author)
+        date=datetime.datetime.now()
+        with open('tickets.json') as f:
+            data = json.load(f)
+            TicketID=len(data)
+            infoW = ' '.join(info)
+            a_dict = {TicketID:{"ID": personID,
+            "User": personName,
+            "Date": str(date),
+            "Ticket": infoW,
+            "RespondedTo": "False",
+            "Responder": "False",
+            "Response": "False",
+            "ResponseDate": "False"}}
+
+            data.update(a_dict)
+
+            with open('tickets.json', 'w') as f:
+                json.dump(data, f)
+
+            with open('tickets.json') as f:
+                data = json.load(f)
+        print(info)
+        ToSend= "\n\n`Ticket ID: ",str(TicketID),"`"
+        ToSend =''.join(ToSend)
+        info = ' '.join(info)
+        ToSend=info+ToSend
+        await channelr.send(ToSend)
+
+@client.command(pass_context=True)
+async def ticketinfo(ctx, ticketID, inputs):
+    if await check_if_server(ctx):
+        try:
+            with open('tickets.json') as f:
+                data = json.load(f)
+                req=data[ticketID][inputs]
+                await ctx.message.channel.send(req)
+        except:
+            await ctx.message.channel.send('Usage: -ticketinfo TICKETID INFO')
+            await ctx.message.channel.send('INFO: User - User, Date - String, Ticket - Integer, RespondedTo - Boolean, Responder - User, Response - String')
+
+@client.command(pass_context=True)
+async def respond(ctx, ticketID, *info):
+    if await check_if_server(ctx):
+        with open('tickets.json') as f:
+            info = ' '.join(info)
+            data = json.load(f)
+            respondeer=str(ctx.message.author.name)
+            personId=data[ticketID]["ID"]
+            personname=data[ticketID]["User"]
+            infoW=data[ticketID]["Ticket"]
+            dateorig=data[ticketID]["Date"]
+            dater=datetime.datetime.now()
+            a_dict = {ticketID:{"ID": personId,
+            "User": personname,
+            "Date": str(dateorig),
+            "Ticket": infoW,
+            "RespondedTo": "True",
+            "Responder": respondeer,
+            "Response": info,
+            "ResponseDate":str(dater)}}
+
+            data.update(a_dict)
+
+            with open('tickets.json', 'w') as f:
+                json.dump(data, f)
+
+            with open('tickets.json') as f:
+                data = json.load(f)
+
+        stron="Response to your ticket with ID: ",ticketID,"\n",info
+        memberpls = discord.utils.get(ctx.message.guild.members, name=personname)
+        print(str(memberpls))
+        await ctx.send(memberpls, str(stron))
+
+@client.command(pass_context=True)
 async def invite(ctx):
     await ctx.message.channel.send("https://discordapp.com/api/oauth2/authorize?client_id=523203714794913802&permissions=8&scope=bot")
 
@@ -72,96 +156,116 @@ def insert_returns(body):
 @client.command()
 async def evaluate(ctx, *, cmd):
     if await check_if_burnt(ctx):
-        fn_name = "_eval_expr"
+        """
+        -evaluate ```
+        a = 1 + 2
+        b = a * 2
+        await ctx.send(a + b)
+        a
+        ```
 
-        cmd = cmd.strip("` ")
+        remember the space between eavluate and ```
+        """
+        try:
+            fn_name = "_eval_expr"
 
-        # add a layer of indentation
-        cmd = "\n".join(f"    {i}" for i in cmd.splitlines())
+            cmd = cmd.strip("` ")
+            cmd = cmd.strip("py")
 
-        # wrap in async def body
-        body = f"async def {fn_name}():\n{cmd}"
+            # add a layer of indentation
+            cmd = "\n".join(f"    {i}" for i in cmd.splitlines())
 
-        parsed = ast.parse(body)
-        body = parsed.body[0].body
+            # wrap in async def body
+            body = f"async def {fn_name}():\n{cmd}"
 
-        insert_returns(body)
+            parsed = ast.parse(body)
+            body = parsed.body[0].body
 
-        env = {
-            'bot': ctx.bot,
-            'discord': discord,
-            'commands': commands,
-            'ctx': ctx,
-            '__import__': __import__
-        }
-        exec(compile(parsed, filename="<ast>", mode="exec"), env)
+            insert_returns(body)
 
-        result = (await eval(f"{fn_name}()", env))
+            env = {
+                'bot': ctx.bot,
+                'discord': discord,
+                'commands': commands,
+                'ctx': ctx,
+                '__import__': __import__
+            }
+            exec(compile(parsed, filename="<ast>", mode="exec"), env)
+            result = (await eval(f"{fn_name}()", env))
+        except Exception as uwuowo:
+            await ctx.message.channel.send(uwuowo)
+    else:
+        await ctx.message.channel.send("This command is made only accessible to Burnt.")
 #EVALUATION CODE END
 
 @client.command(pass_context=True)
 async def roles(ctx):
-    word=""
-    with open("roles.txt","r") as s:
-        for x in s:
-            if x == "":
-                pass
-            else:
-                word=word + x
+    if await check_if_server(ctx):
+        word=""
+        with open("roles.txt","r") as s:
+            for x in s:
+                if x == "":
+                    pass
+                else:
+                    word=word + x
 
-        await ctx.message.channel.send("Available roles:")
-        poo="```"+word+"```"
-        await ctx.message.channel.send(poo)
+            await ctx.message.channel.send("Available roles:")
+            poo="```"+word+"```"
+            await ctx.message.channel.send(poo)
 
 @client.command(pass_context=True)
 async def role(ctx, rolename):
-    go=rolename.capitalize()
-    PINGER=ctx.message.author.mention
-    if go in availableroles:
-        role = discord.utils.get(ctx.message.guild.roles, name=go)
-        if role is None:
-            await ctx.message.channel.send("Role not found")
-        else:
-            if str(role) in [y.name for y in ctx.author.roles]:
-                user=ctx.message.author
-                await user.remove_roles(role)
-                msg=PINGER," removed ",str(role)
-                strong =''.join(msg)
-                await ctx.message.channel.send(strong)
+    if await check_if_server(ctx):
+        go=rolename.capitalize()
+        PINGER=ctx.message.author.mention
+        if go in availableroles:
+            role = discord.utils.get(ctx.message.guild.roles, name=go)
+            if role is None:
+                await ctx.message.channel.send("Role not found")
             else:
-                user=ctx.message.author
-                await user.add_roles(role)
-                msg=PINGER," granted ",str(role)
-                strong =''.join(msg)
-                await ctx.message.channel.send(strong)
+                if str(role) in [y.name for y in ctx.author.roles]:
+                    user=ctx.message.author
+                    await user.remove_roles(role)
+                    msg=PINGER," removed ",str(role)
+                    strong =''.join(msg)
+                    await ctx.message.channel.send(strong)
+                else:
+                    user=ctx.message.author
+                    await user.add_roles(role)
+                    msg=PINGER," granted ",str(role)
+                    strong =''.join(msg)
+                    await ctx.message.channel.send(strong)
     else:
         print("This role hasn't been added using the bot")
         await ctx.message.channel.send("This role hasn't been added using the bot")
 
 @client.command(pass_context=True)
 async def addrole(ctx, rolename):
-    if await check_if_moderator(ctx):
-       role = discord.utils.get(ctx.message.guild.roles, name=rolename)
-       await ctx.guild.create_role(name=rolename)#hi skid.I remade the bot 100% just making a check if user has the mod role stuff
-       with open('roles.txt', 'a') as the_file:
-           strang=str(rolename)+'\n'
-           the_file.write(strang)
-           availableroles.append(rolename)
-           print(availableroles)
-       await ctx.message.channel.send('Success')
+    if await check_if_server(ctx):
+        if await check_if_moderator(ctx):
+           role = discord.utils.get(ctx.message.guild.roles, name=rolename)
+           await ctx.guild.create_role(name=rolename)#hi skid.I remade the bot 100% just making a check if user has the mod role stuff
+           with open('roles.txt', 'a') as the_file:
+               strang=str(rolename)+'\n'
+               the_file.write(strang)
+               availableroles.append(rolename)
+               print(availableroles)
+           await ctx.message.channel.send('Success')
 
 @client.command(pass_context=True)
 async def delrole(ctx, rolename):
-     if await check_if_moderator(ctx):
-         role = discord.utils.get(ctx.message.guild.roles, name=rolename)
-         if role is None:
-             await ctx.message.channel.send("Role not found")
-         else:
-             await role.delete()
-             await ctx.message.channel.send("Role deleted")
-             availableroles.remove(rolename)
-             with open('roles.txt', 'w') as the_file:
-                 for l in availableroles:
-                     the_file.write(str(l)+'\n')
+    if await check_if_server(ctx):
+
+        if await check_if_moderator(ctx):
+            role = discord.utils.get(ctx.message.guild.roles, name=rolename)
+            if role is None:
+                await ctx.message.channel.send("Role not found")
+            else:
+                await role.delete()
+                await ctx.message.channel.send("Role deleted")
+                availableroles.remove(rolename)
+                with open('roles.txt', 'w') as the_file:
+                    for l in availableroles:
+                        the_file.write(str(l)+'\n')
 
 client.run(TOKEN)
